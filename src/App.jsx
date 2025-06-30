4a0421a91acbf4f93ec1940d61d44468533f77d1
Matteo aka. Michael Pitolano <217023450+Caniz-27@users.noreply.github.com>
2025-06-30 14:10:41 +0200

    Finito il design del menu laterale, aggiunta nella landing la sezione "dove trovarci"

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import { useState } from 'react'
 import './App.css'
 import { HiMenuAlt4 } from "react-icons/hi";
+import SideMenu from './components/SideMenu';
+import MenuIcon from './assets/hamburger-icon.svg'; // Assuming you have a menu icon image
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
+    <>
     <div className="landing-container">
 
       {/* Menu Icon */}
-      <div className="menu-icon">
-        <HiMenuAlt4 className='icon' />
+      <div className="menu-icon" onClick={() => setMenuOpen(true)}>
+        <img src={MenuIcon} alt="" className='icon'  />
       </div>
 
       {/* Header */}
@@ -65,7 +68,22 @@ function App() {
           <button type="submit">PRENOTA</button>
         </form>
       </div>
+      <SideMenu isOpen={menuOpen} onClose={() => setMenuOpen(false)} />
     </div>
+    
+    <div className="dove-siamo">
+      <p>VIENICI A TROVARE</p>
+      <div className="map-container">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2850.093741132638!2d8.96357180000001!3d44.410722100000015!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x12d34314cda44299%3A0x86eb7f9f6abc052d!2sAccademia%20Digitale%20Liguria%20ITS%20ICT!5e0!3m2!1sit!2sit!4v1751284873140!5m2!1sit!2sit"
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          title="Mappa Accademia Digitale"
+        ></iframe>
+      </div>
+    </div>
+    </>
   );
 }
 
